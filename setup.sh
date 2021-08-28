02028093e0b19299a17be701b15c179caaf1f9e7
sagarrokad1 <84773742+sagarrokad1@users.noreply.github.com>
2021-08-28 22:56:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"asherholder123@gmail.com"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
