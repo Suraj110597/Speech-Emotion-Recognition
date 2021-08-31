bcec0c2ba101c6baa108fc3e43979b123a788605
sagarrokad1 <84773742+sagarrokad1@users.noreply.github.com>
2021-08-31 23:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 def main():
   @st.cache(allow_output_mutation=True)
   def load_model():
-    model=tf.keras.models.load_model('vgg_19_classifier16.h5')
+    model=tf.keras.models.load_model('Models/vgg_19_classifier16.h5')
     return model
   with st.spinner('Model is being loaded..'):
     model=load_model()
